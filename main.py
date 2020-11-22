e7a171603e29ff4dddc10c6c051356b891b8ec38
zenkoveric <zenkoveric@icloud.com>
2020-11-22 18:22:16 +1000

    added cast_ray function
    added cast_ray function

--- main.py
+++ main.py
@@ -12,16 +12,17 @@ world_map = [
             [1, 1, 1, 1, 1],
             [1, 0, 1, 0, 1],
             [1, 0, 0, 0, 1],
-            [1, 0, 0, 0, 1],
+            [1, 0, 1, 0, 1],
             [1, 1, 1, 1, 1]
             ]
+
 CELLSIZE = 64
 map_width = len(world_map[0]) * CELLSIZE
 map_height = len(world_map) * CELLSIZE
 
 #Init window
-window_width = 800#320
-window_height = 800#180
+window_width = 175#320
+window_height = 175#180
 window = pygame.display.set_mode((window_width, window_height))
 
 #Init plaeyer
@@ -48,7 +49,33 @@ def draw_player(screen, player, width, height):
 
     line_length = 10;
     pygame.draw.circle(screen, (255, 255, 255),(new_x, new_y), 3);
-    pygame.draw.line(screen, (255, 255, 255), (new_x, new_y), (new_x + math.cos(player.direction)*line_length, new_y + math.sin(player.direction)*line_length))
+    # pygame.draw.line(screen, (255, 255, 255), (new_x, new_y), (new_x + math.cos(player.direction)*line_length, new_y + math.sin(player.direction)*line_length))
+    pygame.draw.line(screen, (255, 255, 255), (new_x, new_y), (new_x + math.cos(player.direction+player.FOV/2)*line_length, new_y + math.sin(player.direction+player.FOV/2)*line_length))
+    pygame.draw.line(screen, (255, 255, 255), (new_x, new_y), (new_x + math.cos(player.direction-player.FOV/2)*line_length, new_y + math.sin(player.direction-player.FOV/2)*line_length))
+
+    # Drawing lines
+    '''step = player.FOV / window_width
+    current_angle = player.direction-player.FOV/2
+    for i in range(0, window_width):
+        draw, line_length = player.cast_ray(current_angle, world_map, CELLSIZE)
+        line_length = line_length / map_width * width
+        if draw:
+            pygame.draw.line(screen, (0, 255, 0), (new_x, new_y), (new_x + math.cos(current_angle)*line_length, new_y + math.sin(current_angle)*line_length))
+        current_angle += step
+    '''
+    
+    # print(str(current_angle) + ' ' + str(player.direction + player.FOV/2))
+    '''
+    draw, line_length = player.cast_ray(player.direction, world_map, CELLSIZE)
+    line_length = line_length / map_width * width
+    if draw:
+        pygame.draw.line(screen, (0, 255, 0), (new_x, new_y), (new_x + math.cos(player.direction)*line_length, new_y + math.sin(player.direction)*line_length))
+    '''
+# print(player.cast_ray(0, world_map, CELLSIZE)[0])
+
+def render(screen, player, map):
+    # def cast_ray(self, direction, map, cellSize):
+    pass
 
 #Main loop
 fps = 60
@@ -62,6 +89,7 @@ while run:
     
     draw_map(window, world_map)  
     draw_player(window, player, len(world_map)*35, len(world_map[0])*35);  
+    render(window, player, world_map)
 
     player.move_and_rotate(delta_time, pygame.key.get_pressed())   
     pygame.display.update()
--- player.py
+++ player.py
@@ -1,4 +1,5 @@
 import pygame
+import math
 from vector import Vector2D
 
 class Player:
@@ -20,3 +21,122 @@ class Player:
             self.direction -= self.rotation_speed * delta_time
         if keys[pygame.K_d]:
             self.direction += self.rotation_speed * delta_time
+
+    def clamp_angle(self, angle):
+        if angle < 0:
+            angle = -angle
+            if angle < math.pi:
+                angle = math.pi * 2 - angle
+        if angle > math.pi * 2:
+            angle = angle % (math.pi * 2)
+        return angle
+
+    def cast_ray(self, direction, map, cellSize):
+        angle = self.clamp_angle(direction)
+
+        looks_right = not (math.pi/2 < angle < 3*math.pi/2)
+        looks_up = (math.pi > angle > 0)
+
+        ROV = 10
+
+        is_only_horizontal = angle == math.pi/2 or angle == 3 * math.pi / 2
+        is_only_vertical = angle == math.pi or angle == 0
+
+        # Check horizontal intersections
+        has_horizontal_intersection = False  
+        distance_to_horizontal_intersection = 0
+
+        xa = 0
+        if math.tan(angle) != 0:
+            xa = cellSize / math.tan(angle)
+        ya = cellSize
+        
+        if not looks_right:
+            xa = -xa
+        if looks_up:
+            ya = -ya
+
+        yo = -(self.position.y - (self.position.y // cellSize) * cellSize)
+        if not looks_up:
+            yo = cellSize + yo
+
+        xo = 0
+        if math.tan(angle) != 0:
+            xo = abs(yo) / math.tan(angle)
+        if not looks_right:
+            xo = -xo
+
+        current_x = self.position.x + xo
+        current_y = self.position.y + yo
+
+        if not is_only_vertical:
+            for i in range(0, ROV+1):
+                ix = int(current_x // cellSize)
+                iy = int((current_y // cellSize) - 1)
+                if not looks_up:
+                    iy += 1
+
+                if ix < 0 or iy < 0 or ix > len(map[0])-1 or iy > len(map)-1:
+                    break
+
+                if map[iy][ix] == 1:
+                    has_horizontal_intersection = True
+                    distance_to_horizontal_intersection = (Vector2D(current_x, current_y) - self.position).Magnitude()
+                    break
+
+                current_x += xa
+                current_y += ya
+
+        # Check vertical intersections
+        has_vertical_intersection = False
+        distance_to_vertical_intersection = 0
+        xa = cellSize
+        ya = math.tan(angle) * cellSize
+
+        if looks_up:
+            ya = -ya
+        if not looks_right:
+            xa = -xa
+
+        xo = -(self.position.x - (self.position.x // cellSize) * cellSize)
+        if looks_right: 
+            xo = cellSize + xo
+
+        yo = math.tan(angle) * abs(xo)
+        if not looks_up:
+            yo = -yo
+
+        current_x = self.position.x + xo
+        current_y = self.position.y + yo
+
+        if not is_only_horizontal:
+            for i in range(0, ROV+1):
+                ix = int(current_x // cellSize)
+                iy = int(current_y // cellSize)
+                if not looks_right:
+                    ix -= 1
+
+                if ix < 0 or iy < 0 or ix > len(map[0])-1 or iy > len(map)-1:
+                    break
+
+                if map[iy][ix] == 1:
+                    has_vertical_intersection = True
+                    distance_to_vertical_intersection = (Vector2D(current_x, current_y) - self.position).Magnitude()
+                    break
+
+                current_x += xa
+                current_y += ya
+
+        # print('hor: ' + str(has_horizontal_intersection) + ' ' + str(distance_to_horizontal_intersection))
+        # print('ver: ' + str(has_vertical_intersection) + ' ' + str(distance_to_vertical_intersection))
+
+        # Return values
+        distance = 0
+        if has_horizontal_intersection and has_vertical_intersection:
+            distance = min(distance_to_vertical_intersection, distance_to_horizontal_intersection)
+        elif has_horizontal_intersection and not has_vertical_intersection:
+            distance = distance_to_horizontal_intersection
+        elif not has_horizontal_intersection and has_vertical_intersection:
+            distance = distance_to_vertical_intersection
+
+        return has_horizontal_intersection or has_vertical_intersection, distance
