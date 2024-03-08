4987ed1cfcb4287b848650446b1ac6f56a7625a8
Raydelto Hernandez <raydelto@gmail.com>
2024-03-08 10:07:56 -0400

    Cleaning up code

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from player import Player
 from vector import Vector2D
 
 # Colors
-wallColor  = (43, 82, 121)
+wallColor = (43, 82, 121)
 floorColor = (98, 163, 176)
 
 # Init map and its size
@@ -14,61 +14,61 @@ world_map = [
             [1, 0, 0, 0, 1],
             [1, 0, 1, 0, 1],
             [1, 1, 1, 1, 1]
-            ]
+]
 
 CELLSIZE = 64
 map_width = len(world_map[0]) * CELLSIZE
 map_height = len(world_map) * CELLSIZE
 
-#Init window
-window_width = 320*2#175
-window_height = 180*2#175
+# Init window
+window_width = 320*2  # 175
+window_height = 180*2  # 175
 window = pygame.display.set_mode((window_width, window_height))
 pygame.display.set_caption("ray-casting")
 
-#Init plaeyer
+# Init player
 player_position = Vector2D(map_width/2, map_height/2)
 player = Player(player_position, 80*math.pi/180, 0, 100, 5)
 
+
 def draw_map(screen, map):
-    cellSize = 35 
+    cellSize = 35
 
     for y in range(0, len(map)):
         for x in range(0, len(map[0])):
-            # print(str(y) + str(x))
             color = floorColor
             if map[y][x] == 1:
                 color = wallColor
 
             current_x = x
             current_y = y
-            pygame.draw.rect(screen, color, (current_x*cellSize, current_y*cellSize, current_x+cellSize, current_y+cellSize))
+            pygame.draw.rect(screen, color, (current_x*cellSize, current_y *
+                             cellSize, current_x+cellSize, current_y+cellSize))
+
 
 def draw_player(screen, player, width, height):
     new_x = int(player.position.x / map_width * width)
     new_y = int(player.position.y / map_height * height)
 
     line_length = 10
-    pygame.draw.circle(screen, (255, 255, 255),(new_x, new_y), 3)
-    # pygame.draw.line(screen, (255, 255, 255), (new_x, new_y), (new_x + math.cos(player.direction)*line_length, new_y + math.sin(player.direction)*line_length))
-    pygame.draw.line(screen, (255, 255, 255), (new_x, new_y), (new_x + math.cos(player.direction+player.FOV/2)*line_length, new_y + math.sin(player.direction+player.FOV/2)*line_length))
-    pygame.draw.line(screen, (255, 255, 255), (new_x, new_y), (new_x + math.cos(player.direction-player.FOV/2)*line_length, new_y + math.sin(player.direction-player.FOV/2)*line_length))
+    pygame.draw.circle(screen, (255, 255, 255), (new_x, new_y), 3)
+    pygame.draw.line(screen, (255, 255, 255), (new_x, new_y), (new_x + math.cos(player.direction +
+                     player.FOV/2)*line_length, new_y + math.sin(player.direction+player.FOV/2)*line_length))
+    pygame.draw.line(screen, (255, 255, 255), (new_x, new_y), (new_x + math.cos(player.direction -
+                     player.FOV/2)*line_length, new_y + math.sin(player.direction-player.FOV/2)*line_length))
 
     # Drawing lines
     step = player.FOV / window_width
     current_angle = player.direction-player.FOV/2
     for i in range(0, window_width):
-        draw, line_length, draw_vertical = player.cast_ray(current_angle, world_map, CELLSIZE, screen)
+        draw, line_length, draw_vertical = player.cast_ray(
+            current_angle, world_map, CELLSIZE, screen)
         line_length = line_length / map_width * width
         if draw:
-            pygame.draw.line(screen, (24, 224, 134), (new_x, new_y), (new_x + math.cos(current_angle)*line_length, new_y + math.sin(current_angle)*line_length))
+            pygame.draw.line(screen, (24, 224, 134), (new_x, new_y), (new_x + math.cos(
+                current_angle)*line_length, new_y + math.sin(current_angle)*line_length))
         current_angle += step
-    
 
-    '''draw, line_length = player.cast_ray_2(player.direction, world_map, CELLSIZE, screen)
-    line_length = line_length / map_width * width
-    if draw:
-        pygame.draw.line(screen, (0, 255, 0), (new_x, new_y), (new_x + math.cos(player.direction)*line_length, new_y + math.sin(player.direction)*line_length))'''
 
 def get_interpolated_color(initial_color, final_color, t):
     if t > 1:
@@ -79,18 +79,21 @@ def get_interpolated_color(initial_color, final_color, t):
     new_color = (red, green, blue)
     return new_color
 
-half_cell = CELLSIZE/2
-half_height = window_height/2
+
+half_cell = CELLSIZE / 2
+half_height = window_height / 2
 d = half_cell/math.tan(player.FOV/2)
-initial_color = (0, 159, 66)#(24, 224, 134)#(248, 12, 254)
-final_color = (0, 151, 66)#(121, 0, 156)
+initial_color = (0, 159, 66)
+final_color = (0, 151, 66)
 max_length = CELLSIZE * 2
+
 
 def render(screen, player, map):
     current_angle = player.direction - player.FOV/2
     step = player.FOV/(window_width-1)
     for i in range(0, window_width):
-        draw, length, draw_horizontal = player.cast_ray(current_angle, world_map, CELLSIZE, screen)
+        draw, length, draw_horizontal = player.cast_ray(
+            current_angle, world_map, CELLSIZE, screen)
         h2 = (length/d) * half_cell
         if h2 != 0:
             line_ratio = half_cell/h2
@@ -99,25 +102,32 @@ def render(screen, player, map):
                 color = final_color
                 if draw_horizontal:
                     color = initial_color
-                # color = get_interpolated_color(initial_color, final_color, length/max_length)
-                pygame.draw.line(screen, color, (i, half_height+half_line_length), (i, half_height-half_line_length))
+                pygame.draw.line(screen, color, (i, half_height +
+                                 half_line_length), (i, half_height-half_line_length))
             current_angle += step
-      
-#Main loop
-fps = 60
-clock = pygame.time.Clock()
-run = True
-while run:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            run = False
-    delta_time = clock.tick(fps)/1000
 
-    window.fill((0,0,0))    
-    
-    render(window, player, world_map)
-    draw_map(window, world_map)  
-    draw_player(window, player, len(world_map)*35, len(world_map[0])*35);  
 
-    player.move_and_rotate(delta_time, pygame.key.get_pressed())   
-    pygame.display.update()
+def main():
+    fps = 60
+    clock = pygame.time.Clock()
+    run = True
+    # Main loop
+    while run:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+        delta_time = clock.tick(fps)/1000
+
+        window.fill((0, 0, 0))
+
+        render(window, player, world_map)
+        draw_map(window, world_map)
+        draw_player(window, player, len(world_map)
+                    * 35, len(world_map[0])*35)
+
+        player.move_and_rotate(delta_time, pygame.key.get_pressed())
+        pygame.display.update()
+
+
+if __name__ == "__main__":
+    main()
--- player.py
+++ player.py
@@ -2,6 +2,7 @@ import pygame
 import math
 from vector import Vector2D
 
+
 class Player:
     def __init__(self, position, FOV, direction, movement_speed, rotation_speed):
         self.position = position
@@ -30,7 +31,7 @@ class Player:
     def clamp_angle(self, angle):
         new_angle = 0
         if angle >= 0:
-            new_angle = angle - angle//(2*math.pi) * (2*math.pi) 
+            new_angle = angle - angle//(2*math.pi) * (2*math.pi)
         else:
             pi2 = math.pi * 2
             new_angle = pi2 + ((abs(angle)//pi2)*pi2 + angle)
@@ -78,9 +79,9 @@ class Player:
 
                 if map[iy][ix] == 1:
                     has_horizontal_intersection = True
-                    horizontal_distance = (Vector2D(current_x, current_y) - self.position).Magnitude()
+                    horizontal_distance = (
+                        Vector2D(current_x, current_y) - self.position).Magnitude()
                     break
-
 
                 current_x += xs
                 current_y += ys
@@ -117,7 +118,8 @@ class Player:
 
                 if map[iy][ix] == 1:
                     has_vertical_intersection = True
-                    vertical_distance = (Vector2D(current_x, current_y) - self.position).Magnitude()
+                    vertical_distance = (
+                        Vector2D(current_x, current_y) - self.position).Magnitude()
                     break
 
                 current_x += xs
@@ -135,11 +137,9 @@ class Player:
             if horizontal_distance < vertical_distance:
                 is_horizontal_the_nearest = True
 
-        # Removing destortion 
+        # Removing destortion
         beta = abs(self.clamp_angle(self.direction)-angle)
         if looks_up:
             beta = math.pi*2 - abs(self.clamp_angle(self.direction)-angle)
         distance = distance * math.cos(beta)
-        # return has_horizontal_intersection, horizontal_distance, True
-        # return has_vertical_intersection, vertical_distance, False
         return has_horizontal_intersection or has_vertical_intersection, distance, is_horizontal_the_nearest
